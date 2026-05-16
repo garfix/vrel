8be141c88abb42bc4aa702674eb2e500d7156d90
Patrick van Bergen <patrick.vanbergen@gmail.com>
2026-05-16 20:01:19 +0200

    exec code

--- src/vrel/module/transform/exec_code.py
+++ src/vrel/module/transform/exec_code.py
@@ -3,24 +3,19 @@ from vrel.entity.Atom import Atom
 from vrel.interface.SomeSolver import SomeSolver
 
 
-def exec_code(terms: list, solver: SomeSolver):
-    for term in terms:
-        if isinstance(term, Atom):
-            exec_atom(term, solver)
+def exec_code(term: any, solver: SomeSolver):
+    if isinstance(term, Atom):
+        if len(term.exec) > 0:
+            if term.predicate != PRED_NAME:
+                raise Exception("Exec atoms should be attacheched to `name` atoms")
 
+            solver.solve(term.exec)
 
-def exec_atom(atom: Atom, solver: SomeSolver):
-    if len(atom.exec) > 0:
-        if atom.predicate != PRED_NAME:
-            raise Exception("Exec atoms should be attacheched to `name` atoms")
-
-        solver.solve(atom.exec)
-
-    for arg in atom.arguments:
-        if isinstance(arg, Atom):
-            exec_atom(arg, solver)
-        elif isinstance(arg, list):
+        for arg in term.arguments:
             exec_code(arg, solver)
+        for mod in term.modifiers:
+            exec_code(mod, solver)
 
-    for mod in atom.get_modifier_atoms():
-        exec_atom(mod, solver)
+    elif isinstance(term, list):
+        for element in term:
+            exec_code(element, solver)
