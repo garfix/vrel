5eccc8391f4bb272b7da498688ec6f27de4faf50
Patrick van Bergen <patrick.vanbergen@gmail.com>
2026-05-17 19:57:53 +0200

    grammar

--- tests/integration/pam/read_grammar.py
+++ tests/integration/pam/read_grammar.py
@@ -5,10 +5,6 @@ from vrel.entity.Variable import Variable
 C1 = Variable("C1")
 C2 = Variable("C2")
 C3 = Variable("C3")
-Sub = Variable("Sub")
-Obj = Variable("Obj")
-Obj2 = Variable("Obj2")
-X = Variable("X")
 
 
 def get_read_grammar():
@@ -42,30 +38,37 @@ def get_read_grammar():
         },
         # declarative
         {
-            "syn": "decl(E1) -> np(E1) vp(E1)+'.'",
+            "syn": "decl(C1) -> np(E1) vp(C1, E1)+'.'",
             "sem": lambda np, vp: Atom("intent_understand", vp.mod(np)),
         },
         # verb phrase
-        {"syn": "vp(E1) -> copula() adjp(E1)", "sem": lambda copula, adjp: adjp},
+        {"syn": "vp(C1, E1) -> copula() adjp(E1)", "sem": lambda copula, adjp: adjp},
+        {"syn": "vp(C1, E1) -> vp(C1, E1, E2) np(E2)", "sem": lambda vp, np: vp.mod(np)},
+        {"syn": "vp(C1, E1, E2) -> verb(C1, E1, E2)", "sem": lambda verb: verb},
+        # verb
+        {"syn": "verb(C1, E1, E2) -> 'picked' 'up'", "sem": lambda: Atom("pick_up", C1, E1, E2)},
+        {"syn": "verb(C1, E1, E2) -> 'got' 'into'", "sem": lambda: Atom("get_into", C1, E1, E2)},
+        # adjective phrase
         {"syn": "adjp(E1) -> adj(E1)", "sem": lambda adj: adj},
         {"syn": "adj(E1) -> 'hungry'", "sem": lambda: Atom("hungry", E1)},
+        # copula
         {"syn": "copula() -> 'was'", "sem": lambda: None},
         # noun phrase
         {"syn": "np(E1) -> nbar(E1)", "sem": lambda nbar: nbar},
-        {
-            "syn": "np(E1) -> det(E1) nbar(E1)",
-            "sem": lambda det, nbar: nbar.with_determiner(det),
-        },
+        {"syn": "np(E1) -> pronoun(E2) nbar(E1)", "sem": lambda pronoun, nbar: nbar.mod(pronoun)},
+        {"syn": "np(E1) -> det(E1) nbar(E1)", "sem": lambda det, nbar: nbar.with_determiner(det)},
         # nbar
         {"syn": "nbar(E1) -> noun(E1)", "sem": lambda noun: noun},
+        {"syn": "nbar(E1) -> pronoun(E1)", "sem": lambda pronoun: pronoun},
+        # determiner
+        {"syn": "det(E1) -> 'the'", "sem": lambda: None},
         # noun
-        {
-            "syn": "noun(E1) -> proper_noun(E1)",
-            "sem": lambda proper_noun: proper_noun,
-        },
+        {"syn": "noun(E1) -> proper_noun(E1)", "sem": lambda proper_noun: proper_noun},
+        {"syn": "noun(E1) -> 'michelin' 'guide'", "sem": lambda: Atom("michelin_guide", E1)},
+        {"syn": "noun(E1) -> 'car'", "sem": lambda: Atom("car", E1)},
+        # pronoun
+        {"syn": "pronoun(E1) -> 'she'", "sem": lambda: Atom("she", E1)},
+        {"syn": "pronoun(E1) -> 'her'", "sem": lambda: Atom("her", E1)},
         # proper noun
-        {
-            "syn": "proper_noun(E1) -> /\\w+/",
-            "sem": lambda token: Atom("name", E1, token),
-        },
+        {"syn": "proper_noun(E1) -> /\\w+/", "sem": lambda token: Atom("name", E1, token)},
     ]
